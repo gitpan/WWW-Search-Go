1ef15c9c06ce2e4b4b71084be3db35d236bc4af9
Alain Barbet <alb@amisw.com>
2002-08-10 11:54:47 +0000

    Import of ALIAN/WWW-Search-Go-1.3 from CPAN.
    gitpan-cpan-distribution: WWW-Search-Go
    gitpan-cpan-version:      1.3
    gitpan-cpan-path:         ALIAN/WWW-Search-Go-1.3.tar.gz
    gitpan-cpan-author:       ALIAN
    gitpan-cpan-maturity:     released

--- Go.pm
+++ Go.pm
@@ -1,140 +1,131 @@
 #
 # Go.pm by Alain Barbet alian@alianwebserver.com
 # Copyright (C) 2000
-# $Id: Go.pm,v 1.2 2001/10/02 21:08:28 alian Exp $
+# $Id: Go.pm,v 1.3 2002/08/10 11:40:30 alian Exp $
 #
 
 package WWW::Search::Go;
-
+use strict;
 use vars qw($VERSION @ISA @EXPORT @EXPORT_OK);
-
 require Exporter;
+
 @EXPORT = qw();
 @EXPORT_OK = qw();
 @ISA = qw(WWW::Search Exporter);
 
-$VERSION = ('$Revision: 1.2 $ ' =~ /(\d+\.\d+)/)[0];
+$VERSION = ('$Revision: 1.3 $ ' =~ /(\d+\.\d+)/)[0];
 
 use Carp ();
-use strict "vars";
-use WWW::Search(generic_option);
+use WWW::Search(qw (generic_option strip_tags));
 require WWW::SearchResult;
 
 # private
-sub native_setup_search
-	{
-    	my($self, $native_query, $native_options_ref) = @_;
-    	$self->user_agent('alian');
-    	$self->{_next_to_retrieve} = 0;
-	#$self->{_debug} = 1;
-	# options par defaut
-	if (!defined($self->{_options})) {
-	$self->{_options} =
-	  {
-	   'Partner' 	=> 'go_home',
-	   'Keywords' 	=> $native_query,
-	   'Go' 	      => 'Search',
-	   'search_url'   => 'http://www.goto.com/d/search/p/go/',
-        };}
+sub native_setup_search	{
+  my($self, $native_query, $native_options_ref) = @_;
+  $self->user_agent('alian');
+  $self->{_next_to_retrieve} = 0;
+  #$self->{_debug} = 1;
+  # options par defaut
+  if (!defined($self->{_options})) {
+    $self->{_options} =
+      {
+       'Partner' 	=> 'go_home',
+       'Keywords' 	=> $native_query,
+       'Go' 	      => 'Search',
+       'search_url'   => 'http://www.goto.com/d/search/p/go/',
+      };}
 
-    	my($options_ref) = $self->{_options};
-    	if (defined($native_options_ref)) 
-    		{
-		# Copy in new options.
-		foreach (keys %$native_options_ref) 
-		  {$options_ref->{$_} = $native_options_ref->{$_};}
-    		}
-    	# Process the options.
-    	# (Now in sorted order for consistency regarless of hash ordering.)
-    	my($options) = '';
-    	foreach (sort keys %$options_ref) 
-    		{
-		# printf STDERR "option: $_ is " . $options_ref->{$_} . "\n";
- 		next if (generic_option($_));
-		$options .= $_ . '=' . $options_ref->{$_} . '&' 
-		  if (defined $options_ref->{$_});
-    		}
-
-    	# Finally figure out the url.
-    	$self->{_base_url} = $self->{_next_url} 
-	  = $self->{_options}{'search_url'} ."?" . $options;
-	}
-
-# private
-sub create_hit
-	{
-	my ($self,$url,$titre,$description)=@_;
-	my $hit = new WWW::SearchResult;
-	$hit->add_url($url);
-	$hit->title($titre);
-	$hit->description($description);
-	push(@{$self->{cache}},$hit);
-	return 1;
-	}
-
-# private
-sub native_retrieve_some
-	{
- 	my ($self) = @_;
-	my($hits_found) = 0;
-	my ($buf,$langue);
-
- 	#fast exit if already done
-	return undef if (!defined($self->{_next_url}));    
-	print STDERR "WWW::Search::Go::native_retrieve_some: fetching " .
-	  $self->{_next_url} . "\n" if ($self->{_debug});
-	my($response) = $self->http_request('GET', $self->{_next_url});
-	$self->{response} = $response;
-	print STDERR "WWW::Search::Go GET  $self->{_next_url} return ",
-	  $response->code,"\n"  if ($self->{_debug});
-  	if (!$response->is_success) {return undef;};
-	$self->{_next_url} = undef; 
-
-    	# parse the output
-    	my($url,$titre,$description);
-    	my @l = split(/<a name=\d*>/, $response->content());
-	foreach my $buf (@l)
-	  {
-	    #print STDERR $buf,"\n***************************\n\n\n";		
-	    my @l2 = split(/\n/, $buf);
-	    foreach my $ligne (@l2)
-		{
-		  chomp $ligne;
-		  # url + titre
-		  if ($ligne=~/^\s*<a href="(.*?)" target="_top">(.*?)<\/a>$/)
-		    {
-			($url, $titre) = ($1, $2);
-		    }
-		  # description
-		  elsif ($ligne=~/^\s*<td><font\sface="verdana,sans-serif"\s
-                             size=2><a\shref=".*"\sclass=x\starget="_top">
-			            (.*)<\/a>$/x)
-		    { $description = $1; }
-		}
-	    # Ajout d'une entree
-	    if ($url && $titre && $description)
-		{
-		  print " *** Find hit: \n\turl: $url\n\ttitre: $titre\n"
-		    if ($self->{_debug});
-		  print "\t description: $description\n"
-		    if ($self->{_debug});
-		  $hits_found+=$self->create_hit($url,$titre,$description);
-		}
-	  }
-	#
-	# NEXT URL
-	#
-	# On a le lien dans le dernier bloc analyse precedement
-	my $buf = $l[$#l];
-	if ($buf=~/<BR><table \s width=100%><tr><td \s align=right>
-	    <a \s href="(.*)"><font \s face="verdana,sans-serif" \s size=1><b>/x)
-	  {
-	    $self->{_next_url} = "http://www.goto.com".$1;
-	    print " *** Next url: $self->{_next_url}\n" if ($self->{_debug});
-	  }
-	$self->approximate_result_count($hits_found);
-	return $hits_found;
+  my($options_ref) = $self->{_options};
+  if (defined($native_options_ref)) 
+    {
+      # Copy in new options.
+      foreach (keys %$native_options_ref) 
+	{$options_ref->{$_} = $native_options_ref->{$_};}
     }
+  # Process the options.
+  # (Now in sorted order for consistency regarless of hash ordering.)
+  my($options) = '';
+  foreach (sort keys %$options_ref) 
+    {
+      # printf STDERR "option: $_ is " . $options_ref->{$_} . "\n";
+      next if (generic_option($_));
+      $options .= $_ . '=' . $options_ref->{$_} . '&' 
+	if (defined $options_ref->{$_});
+    }
+
+  # Finally figure out the url.
+  $self->{_base_url} = $self->{_next_url} 
+    = $self->{_options}{'search_url'} ."?" . $options;
+}
+
+# private
+sub create_hit	{
+  my ($self,$url,$titre,$description)=@_;
+  my $hit = new WWW::SearchResult;
+  $hit->add_url($url);
+  $hit->title($titre);
+  $hit->description($description);
+  push(@{$self->{cache}},$hit);
+  return 1;
+}
+
+# private
+sub native_retrieve_some	{
+  my ($self) = @_;
+  my($hits_found) = 0;
+  my ($buf,$langue);
+
+  #fast exit if already done
+  return undef if (!defined($self->{_next_url}));    
+  print STDERR "WWW::Search::Go::native_retrieve_some: fetching " .
+    $self->{_next_url} . "\n" if ($self->{_debug});
+  my($response) = $self->http_request('GET', $self->{_next_url});
+  $self->{response} = $response;
+  print STDERR "WWW::Search::Go GET  $self->{_next_url} return ",
+    $response->code,"\n"  if ($self->{_debug});
+  if (!$response->is_success) {return undef;};
+  $self->{_next_url} = undef; 
+  
+  # parse the output
+  my($url,$titre,$description);
+  my @l = split(/<a name=\d*>/, $response->content());
+  foreach my $buf (@l)
+    {
+      #print STDERR $buf,"\n***************************\n\n\n";		
+      my @l2 = split(/\n/, $buf);
+      foreach my $ligne (@l2) {
+	chomp $ligne;
+	# url + titre
+	if ($ligne=~/<a href="(.*?)" target="_top">(.*?)<\/a>$/) {
+	  ($url, $titre) = ($1, $2);
+	}
+	# description
+	elsif ($ligne=~/^\s* <td><font \s face="verdana,sans-serif"
+	       \s size=2>(.*)$/x) { $description = $1; }
+      }
+      # Ajout d'une entree
+      if ($url && $titre && $description) {
+	print " *** Find hit: \n\turl: $url\n\ttitre: $titre\n"
+	  if ($self->{_debug});
+	print "\t description: $description\n"
+	  if ($self->{_debug});
+	$hits_found+=$self->create_hit($url,$titre,$description);
+      }
+    }
+  #
+  # NEXT URL
+  #
+  # On a le lien dans le dernier bloc analyse precedement
+  my $buf = $l[$#l];
+  if ($buf=~/<BR><table \s width=100%><tr><td \s align=right>
+      <a \s href="(.*)"><font \s face="verdana,sans-serif" \s size=1><b>/x)
+    {
+      $self->{_next_url} = "http://www.goto.com".$1;
+      print " *** Next url: $self->{_next_url}\n" if ($self->{_debug});
+    }
+  $self->approximate_result_count($hits_found);
+  return $hits_found;
+}
 
 1;
 
--- test.pl
+++ test.pl
@@ -2,33 +2,32 @@
 # `make test'. After `make install' it should work as `perl test.pl'
 
 use ExtUtils::testlib;
+use WWW::Search::Test;
 
 ######################### We start with some black magic to print on failure.
 
 # Change 1..1 below to 1..last_test_to_print .
 # (It may become useful if the test is moved to ./t subdirectory.)
 
-# 6 tests without "goto MULTI_RESULT"
-BEGIN { $| = 1; print "1..5\n"; }
+# 6 tests 
+BEGIN { $| = 1; print "1..6\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use WWW::Search::Go;
 $loaded = 1;
-print "ok 1\n";
+print "ok 1 Load test\n";
 
 ######################### End of black magic.
 
-# Insert your test code below (better if it prints "ok 13"
-# (correspondingly "not ok 13") depending on the success of chunk 13
-# of the test code):
-
+my $tt = 6;
+my $ok = 1;
 my $iTest = 2;
 
 my $sEngine = 'Go';
 my $oSearch = new WWW::Search($sEngine);
 print ref($oSearch) ? '' : 'not ';
-print "ok $iTest\n";
+print "ok $iTest WWW::Search compliant\n";
+$ok++ if (ref($oSearch));
 
-use WWW::Search::Test;
 my $debug = 0;
 
 # This test returns no results (but we should not get an HTTP error):
@@ -37,9 +36,9 @@ $oSearch->native_query($WWW::Search::Test::bogus_query);
 @aoResults = $oSearch->results();
 $iResults = scalar(@aoResults);
 print STDOUT (0 < $iResults) ? 'not ' : '';
-print "ok $iTest\n";
-
-# goto MULTI_RESULT;
+print "ok $iTest Bogus query\n";
+$ok++ if ($iResults==0);
+sleep(2);
 
 # This query returns 1 page of results:
 $iTest++;
@@ -54,27 +53,28 @@ if (($iResults < 2) || (49 < $iResults))
   print STDERR " --- got $iResults results for $sQuery, but expected 2..49\n";
   print STDOUT 'not ';
   }
-print "ok $iTest\n";
+else { $ok++; }
+print "ok $iTest Query $sQuery results 2<x<50\n";
 
-goto MULTI_RESULT;
+sleep(2);
 
 # This query returns 2 pages of results:
 $iTest++;
-$sQuery = 'simpson';
+$sQuery = 'alien';
 $oSearch->native_query(WWW::Search::escape_query($sQuery),
                          { 'search_debug' => $debug, },
                       );
 @aoResults = $oSearch->results();
+$oSearch->maximum_to_retrieve(120);
 $iResults = scalar(@aoResults);
 if (($iResults < 51) || (99 < $iResults))
   {
   print STDERR " --- got $iResults results for $sQuery, but expected 51..99\n";
   print STDOUT 'not ';
   }
-print "ok $iTest\n";
-
-MULTI_RESULT:
-# $debug = 1;
+else { $ok++; }
+print "ok $iTest Query $sQuery results 50<x<100\n";
+sleep(2);
 
 # This query returns 3 pages of results:
 $iTest++;
@@ -90,4 +90,11 @@ if ($iResults < 101)
   print STDERR " --- got $iResults results for $sQuery, but expected > 101\n";
   print STDOUT 'not ';
   }
-print "ok $iTest\n";
+else { $ok++; }
+print "ok $iTest Query $sQuery results x>100\n";
+
+# Code sortie final
+my $ex = 0;
+if ($tt != $ok) { $ex=-1; print "Some tests fails\n"; }
+else { print "All tests successful\n"; }
+exit($ex);
